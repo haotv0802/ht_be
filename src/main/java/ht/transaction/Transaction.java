6df2a9c0f0eee36554e4c0eac0bbb6c9b151c5a8
HaoHo <haho@codix.eu>
2017-04-21 16:58:59 +0700

    Add Transaction Management.

---
+++ src/main/java/ht/transaction/Transaction.java
@@ -0,0 +1,17 @@
+package ht.transaction;
+
+public class Transaction {
+  private String txId;
+
+  public Transaction() {
+  }
+
+  public void setTxId(String txId) {
+    this.txId = txId;
+  }
+
+  public String getTxId() {
+    return txId;
+  }
+}
+
