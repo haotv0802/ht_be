31d0ba8eef889488ad9b60146db52d40d0257650
haho <haotv0802@gmail.com>
2017-06-05 16:00:52 +0700

    Testing updateRoomType.

--- src/test/java/ht/api/rest/admin/rooms/RoomsResourceTest.java
+++ src/test/java/ht/api/rest/admin/rooms/RoomsResourceTest.java
@@ -1,14 +1,17 @@
 package ht.api.rest.admin.rooms;
 
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import ht.api.rest.BaseDocumentation;
 import ht.api.rest.admin.rooms.beans.RoomTypeBean;
-import org.json.JSONArray;
+import io.jsonwebtoken.lang.Assert;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -40,7 +43,43 @@ public class RoomsResourceTest extends BaseDocumentation {
 
     String resultAsString = result.getResponse().getContentAsString();
 
-    JSONArray array = new JSONArray(resultAsString);
-    List<RoomTypeBean> roomTypes = this.objectMapper.convertValue(array, List.class);
+//    JSONArray array = new JSONArray(resultAsString);
+    List<RoomTypeBean> roomTypes =
+        this.objectMapper.readValue(
+            resultAsString,
+            TypeFactory.defaultInstance().constructCollectionType(List.class, RoomTypeBean.class)
+        );
+
+    RoomTypeBean roomType = roomTypes.get(0);
+
+    roomType.setNumOfPeople(11);
+
+    mockMvc
+        .perform(patch("/svc/admin/rooms/roomTypes/update")
+            .header("Accept-Language", "en")
+            .header("X-AUTH-TOKEN", authTokenService.getAuthToken())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(this.objectMapper.writeValueAsString(roomType))
+        )
+        .andExpect(status().is(200))
+    ;
+
+    // check again to make sure data is updated
+    result = mockMvc
+        .perform(get("/svc/admin/rooms/roomTypes")
+            .header("Accept-Language", "en")
+            .header("X-AUTH-TOKEN", authTokenService.getAuthToken())
+        )
+        .andExpect(status().is(200))
+        .andReturn()
+    ;
+    roomTypes =
+        this.objectMapper.readValue(
+            result.getResponse().getContentAsString(),
+            TypeFactory.defaultInstance().constructCollectionType(List.class, RoomTypeBean.class)
+        );
+    roomType = roomTypes.get(0);
+    Assert.notNull(roomType);
+    Assert.isTrue(roomType.getNumOfPeople() == 11);
   }
 }
